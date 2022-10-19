3f7aa227d00d3b8ccd33c2729ef3462199643f33
OussamaAlouat <al317257@uji.es>
2022-10-19 22:39:16 +0200

    Add functionality to search client with cuts and draw it on board

--- src/containers/clientsHluz/ClientsContainer.vue
+++ src/containers/clientsHluz/ClientsContainer.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <search-input></search-input>
-    <client-hluz></client-hluz>
+    <search-input @cupsToSearch="setCupsToSearch"></search-input>
+    <div v-for="(client, i) in clientsWithCuts" :key="i">
+      <client-hluz
+        :key="i"
+        :clientName="client.full_name"
+        :address="client.address"
+        :role="client.role"
+        :buildingType="client.building_type"
+      >
+    </client-hluz>
+    </div>
   </div>
 </template>
 
@@ -15,11 +24,25 @@ export default {
     ClientHluz,
     SearchInput,
   },
+  data: () => ({
+    clientsWithCuts: [],
+  }),
   mounted() {
     axios.get('http://localhost:3000/clients').then((result) => {
       console.log(result.data);
     });
   },
+  methods: {
+    setCupsToSearch(event) {
+      console.log(event);
+
+      axios.get(`http://localhost:3000/clients?cups=${event}`).then((result) => {
+        this.clientsWithCuts = JSON.parse(JSON.stringify(result.data));
+        console.log('Result: ', result.data);
+      });
+    },
+  },
+
 };
 </script>
 
