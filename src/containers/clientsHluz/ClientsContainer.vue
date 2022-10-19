230ee22715b78a06bdc4fde913e3996dc7be99cb
OussamaAlouat <al317257@uji.es>
2022-10-19 17:48:42 +0200

    Add container to manage the info

---
+++ src/containers/clientsHluz/ClientsContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <search-input></search-input>
+    <client-hluz></client-hluz>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ClientHluz from '@/components/ClientHluz.vue';
+import SearchInput from '@/components/SearchInput.vue';
+
+export default {
+  components: {
+    ClientHluz,
+    SearchInput,
+  },
+  mounted() {
+    axios.get('http://localhost:3000/clients').then((result) => {
+      console.log(result.data);
+    });
+  },
+};
+</script>
+
+<style>
+
+</style>
