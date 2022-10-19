4022dd3786016cfd31115d5844fba176aa440d19
OussamaAlouat <al317257@uji.es>
2022-10-19 22:05:21 +0200

    Add new tests for search input component

---
+++ tests/unit/components/SearchInput.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils';
+import SearchInput from '@/components/SearchInput.vue';
+
+describe('SearchInput.vue', () => {
+  it('Component render', () => {
+    const wrapper = shallowMount(SearchInput, {});
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('On click on searchCUPS function will emit "cupsToSearch" event', () => {
+    const wrapper = shallowMount(SearchInput, {});
+    wrapper.vm.searchCUPS();
+    expect(wrapper.emitted()).toHaveProperty('cupsToSearch');
+  })
+});
