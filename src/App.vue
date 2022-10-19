e95560eac576bec1ed2d6dbd8298044d6033b77f
Oussama Alouat <ousalo@CORP.ROOT.GLOBAL>
2022-10-19 15:31:17 +0200

    Clean initial app, to start with the exercise

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    WELLCOME TO THE HLUZ APP
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
   },
 };
 </script>
