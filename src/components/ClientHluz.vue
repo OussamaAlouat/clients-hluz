ce87c6b748999b5f3404fa4f284397d2cf3e8e62
OussamaAlouat <al317257@uji.es>
2022-10-19 17:48:06 +0200

    Add component of client with a card, to display the info

--- src/components/ClientHluz.vue
+++ src/components/ClientHluz.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    Wellcome to Client view
-  </div>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          Cecil Grant
+        </v-list-item-title>
+        <v-list-item-subtitle>Sweet potato street, 2</v-list-item-subtitle>
+        <v-list-text>customer-basic</v-list-text>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey">Build</v-list-item-avatar>
+    </v-list-item>
+  </v-card>
 </template>
 
 <script>
+
 export default {
   name: 'ClientHluz',
+  props: {
+    clientName: {
+      type: String,
+      required: true,
+    },
+
+    adress: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+    },
+    buildingType: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
