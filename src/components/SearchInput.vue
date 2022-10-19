98e18ace76c67cf5d58d6efe70f566c7fe2f9b83
OussamaAlouat <al317257@uji.es>
2022-10-19 17:48:26 +0200

    Add searchInput component to search the cups

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,54 @@
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+  <div  class="input-container">
+    <div style="width:80%">
+      <v-text-field
+        placeholder="Set cups"
+        outlined
+        v-model="cupsValue"
+        class="input-container__input-hluz"
+        type="text"
+        id="cups"
+      >
+        <template v-slot:append>
+          <v-hover v-slot="{ hover }">
+            <v-icon large :color="hover ? 'rgba(229, 0, 126, 255)' : ''" @click="searchCUPS()">
+              mdi-magnify
+            </v-icon>
+          </v-hover>
+        </template>
+      </v-text-field>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cupsValue: '',
+  }),
+
+  methods: {
+    searchCUPS() {
+      this.$emit('cupsToSearch', this.cupsValue);
+      console.log('cups: ', this.cupsValue);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.input-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+
+  &__input-hluz {
+    border-radius: 10px;
+    height: 60px;
+    justify-content: center;
+  }
+}
+</style>
