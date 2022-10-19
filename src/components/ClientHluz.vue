3c8fdf9bf92a4649a8028ba60fccd7dbdb0f4546
OussamaAlouat <al317257@uji.es>
2022-10-20 01:20:28 +0200

    Change client representation

--- src/components/ClientHluz.vue
+++ src/components/ClientHluz.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-card class="mx-auto" max-width="344" outlined>
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title class="text-h5 mb-1">
+    <v-card class="mx-auto" max-width="340">
+      <v-card-text>
+        <p class="text-h4 text--primary">
           {{ clientName }}
-        </v-list-item-title>
-        <v-list-item-subtitle>{{ address }}</v-list-item-subtitle>
-        <v-list-item-subtitle>{{ role }}</v-list-item-subtitle>
-      </v-list-item-content>
+        </p>
 
-      <v-list-item-avatar tile size="80" color="grey">
-        {{ buildingType }}
-      </v-list-item-avatar>
-    </v-list-item>
-  </v-card>
+        <p class="text--primary">{{ address }}</p>
+        <p class="text--primary">{{ role }}</p>
+        <p>Type: {{ buildingType }}</p>
+
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="deep-purple accent-4">
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
 </template>
 
 <script>
@@ -46,9 +48,15 @@ export default {
     neighbors: {
       type: Array,
       required: true,
+      default: () => [],
     },
   },
+
+  computed: {
+
+  },
 };
+
 </script>
 
 <style>
