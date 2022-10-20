6702cf91864416ee7d3665f4452e939d0505e9a1
OussamaAlouat <al317257@uji.es>
2022-10-20 15:24:45 +0200

    Added configurations for tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  verbose: false,
   preset: '@vue/cli-plugin-unit-jest',
   setupFiles: ['./tests/setup.js'],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+  ],
+
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+  },
 };
