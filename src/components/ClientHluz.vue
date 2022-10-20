a18d20505d7dd54b954baf734000765f9925d879
OussamaAlouat <al317257@uji.es>
2022-10-20 09:30:38 +0200

    Remove extra code on clientHluz component

--- src/components/ClientHluz.vue
+++ src/components/ClientHluz.vue
@@ -1,25 +1,18 @@
 <template>
-    <v-card class="mx-auto" max-width="340">
-      <v-card-text>
-        <p class="text-h4 text--primary">
-          {{ clientName }}
-        </p>
+  <v-card class="mx-auto bg-main" max-width="340">
+    <v-card-text>
+      <p class="text-h4 text--primary">
+        {{ clientName }}
+      </p>
 
-        <p class="text--primary">{{ address }}</p>
-        <p class="text--primary">{{ role }}</p>
-        <p>Type: {{ buildingType }}</p>
-
-      </v-card-text>
-      <v-card-actions>
-        <v-btn text color="deep-purple accent-4">
-          Learn More
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+      <p class="text--primary">{{ address }}</p>
+      <p class="text--primary">{{ role }}</p>
+      <p>Type: {{ buildingType }}</p>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-
 export default {
   name: 'ClientHluz',
   props: {
@@ -52,12 +45,9 @@ export default {
     },
   },
 
-  computed: {
-
-  },
+  computed: {},
 };
-
 </script>
 
-<style>
+<style lang="scss">
 </style>
