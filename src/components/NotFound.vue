a9c35e5d7344246993de7bb73efa112c510e811c
OussamaAlouat <al317257@uji.es>
2022-10-20 08:04:43 +0200

    Added new component to display not found CUTS

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-alert type="info">
+      There are no clients for that CUT
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style>
+
+</style>
