652a9fba4e769e89ea8b67817c6b04093a3bbc62
OussamaAlouat <al317257@uji.es>
2022-10-21 08:29:31 +0200

    Add new offer componet and refactor container

---
+++ src/components/OfferHluz.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="mx-auto bg-main" max-width="340">
+    <v-card-text>
+      <P class="text-h4 text--primary">
+        {{ type }}
+      </P>
+
+      <p class="text-h5 text--primary">
+        {{ amount }}
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'OfferComponent',
+  props: {
+    amount: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/containers/clientsHluz/ClientsContainer.vue
+++ src/containers/clientsHluz/ClientsContainer.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="h-100">
     <search-input @cupsToSearch="setCupsToSearch"></search-input>
-    <div v-for="(client, i) in clientsRooftopRevolution" :key="i">
+    <div
+      v-for="(client, i) in clientsRooftopRevolution"
+      :key="i"
+      class="d-flex justify-center mb-10">
       <client-hluz
         :key="i"
         :clientName="client.full_name"
         :address="client.address"
         :role="client.role"
         :buildingType="client.building_type"
-        :neighbors="supplyPoint.neighbors"
-        :tarif="supplyPoint.tariff"
-        :power="supplyPoint.power"
-        :amount="supplyPoint.invoiced_amount"
       >
       </client-hluz>
+    </div>
+
+    <div v-for="(amount, index) in amounts" :key="index">
+      <offer-hluz :amount="amount.amount" :type="amount.type"></offer-hluz>
     </div>
 
     <div v-if="showNotFoundClients" class="h-100 not-found-container">
@@ -28,18 +31,22 @@ import api from '@/services/api';
 import ClientHluz from '@/components/ClientHluz.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import NotFound from '@/components/NotFound.vue';
+import OfferHluz from '@/components/OfferHluz.vue';
 
 export default {
   components: {
     ClientHluz,
     SearchInput,
     NotFound,
+    OfferHluz,
   },
 
   data: () => ({
     clientsWithCuts: [],
     supplyPoint: {},
     searched: false,
+    neighborsInfo: [],
+    amounts: [],
   }),
   mounted() {
     api.getAllClients().then((result) => {
@@ -48,20 +55,44 @@ export default {
   },
   methods: {
     setCupsToSearch(event) {
-      api.getClientsWithCups(event).then((result) => {
-        this.clientsWithCuts = JSON.parse(JSON.stringify(result.data));
-        console.log('Result: ', result.data);
-      });
+      const clients = api.getClientsWithCups(event);
+      const supplyPointPromise = api.getSupplyPoints(event);
+      Promise.all([clients, supplyPointPromise]).then((values) => {
+        // eslint-disable-next-line prefer-destructuring
+        this.clientsWithCuts = values[0].data;
+        console.log('Values on promise: ', values);
 
-      api.getSupplyPoints(event).then((result) => {
-        if (result.data && result.data.length > 0) {
-          this.supplyPoint = JSON.parse(JSON.stringify(result.data[0]));
+        const resultOfSuplyPromise = values[1];
+        if (resultOfSuplyPromise.data && resultOfSuplyPromise.data.length > 0) {
+        // eslint-disable-next-line prefer-destructuring
+          this.supplyPoint = resultOfSuplyPromise.data[0];
+          this.amounts.push({
+            type: 'Standard offer',
+            amount: this.supplyPoint.invoiced_amount,
+          });
+
+          this.getAllNeighborsInformation();
         }
 
-        console.log('suplyPoint: ', result.data[0]);
+        // eslint-disable-next-line prefer-destructuring
+        this.searched = true;
       });
+    },
 
-      this.searched = true;
+    getAllNeighborsInformation() {
+      if (this.supplyPoint && this.supplyPoint.neighbors) {
+        this.supplyPoint.neighbors.forEach(async (elem) => {
+          const value = await this.getNeighborsInformation(elem);
+          this.neighborsInfo.push(value);
+        });
+      }
+    },
+
+    async getNeighborsInformation(neighborCups) {
+      console.log('llama aqui: ', neighborCups);
+      const neighbor = await api.getSupplyPoints(neighborCups);
+      console.log('neighbor: ', neighbor.data);
+      return neighbor.data;
     },
   },
   computed: {
