46d52a6cae955bbfe66beaa58feb47f7ef3ee45e
OussamaAlouat <al317257@uji.es>
2022-10-21 12:18:38 +0200

    Add icon to offer Component

--- src/components/OfferHluz.vue
+++ src/components/OfferHluz.vue
@@ -1,6 +1,9 @@
 <template>
-  <v-card class="mx-auto bg-main" max-width="340">
+  <v-card class="bg-main" max-width="340">
     <v-card-text>
+      <div class="icon-box">
+        <v-icon large color="rgba(245, 141, 27, 255)">{{icon}}</v-icon>
+      </div>
       <P class="text-h4 text--primary">
         {{ type }}
       </P>
@@ -24,10 +27,18 @@ export default {
       type: String,
       required: true,
     },
+    icon: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <style>
-
+.icon-box {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
 </style>
